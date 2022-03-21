8cc04d5ebd01aaa5f19f5852d689421c49d90e79
StjepanR <sr51641@fer.hr>
2022-03-21 20:59:54 +0100

    initial project commit

---
+++ MovieApp/MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Stjepan Ruklić on 21.03.2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
