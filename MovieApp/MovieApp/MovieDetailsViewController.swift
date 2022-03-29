dc2c55d41cf1c1b30f484baa140d630524e8a721
StjepanR <sr51641@fer.hr>
2022-03-30 00:19:16 +0200

    Add Root View Controller

---
+++ MovieApp/MovieApp/MovieDetailsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDetailsViewController.swift
+//  MovieApp
+//
+//  Created by Stjepan Ruklić on 30.03.2022..
+//
+
+import Foundation
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
